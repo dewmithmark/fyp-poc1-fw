edfcc0635effd92cb9041c9888d37e14f4209d7c
dewmithmark <100600233+dewmithmark@users.noreply.github.com>
2023-06-19 17:51:32 +0530

    Added layer folders

---
+++ fyp-poc-fw/Application/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ * Created: 6/19/2023 
+ *  Author: Dewmith Ranasinghe
+ */ 
+
+#include "main.h"
+
+int main(void)
+{
+
+	while (1)
+	{
+      
+	}
+
+	return 0;
+}
---
+++ fyp-poc-fw/Application/main.h
@@ -0,0 +1,16 @@
+/*
+ * main.h
+ *
+ * Created: 6/19/2023 
+ *  Author: dewmithr
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <xc.h>
+
+
+
+#endif /* MAIN_H_ */
